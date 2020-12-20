70b19119528c6ed4529d4c21f7ad0d490839b06d
kyedonghwan-linux <kdh643737@gmail.com>
2020-12-20 22:14:53 +0900

    20201220-2

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -90,24 +90,3 @@ export const deleteVideo = async (req, res) => {
 }
 
 
-/*
-export const search = async (req, res) => {
-    const {
-      query: { term: searchingBy },
-    } = req;
-    let videos = [];
-    try {
-      videos = await Video.find({
-        title: { $regex: searchingBy, $options: "i" },
-      });
-    } catch (error) {
-      console.log(error);
-    }
-    //const searchingBy = req.query.term;
-    res.render("search", {
-      pageTitle: `Search: '${searchingBy}'`,
-      searchingBy,
-      videos,
-    });
-  };
-  */
